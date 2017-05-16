7d616ca61b5ee11fc3f7411f49bd3aa3ea8db3e6
NastyJoke <renaud.costa84@gmail.com>
2017-05-16 17:13:21 +0200

    merge

--- src/main.c
+++ src/main.c
@@ -852,7 +852,7 @@ void display_billboarding_minimap(void){
 	glEnable(GL_DEPTH_TEST);
 }
 ///CHECKPOINT COUNT
-/*
+
 unsigned int vertexArrayObjID3;
 void init_billboarding_cp(void){
 
@@ -864,7 +864,6 @@ void init_billboarding_cp(void){
 												0.52f,1.0f,0.0f};
 
 				//Color Matrix
-<<<<<<< HEAD
 
 	// two vertex buffer objects, used for uploading the
 	unsigned int vertexBufferObjID3;
@@ -924,74 +923,11 @@ void display_billboarding_minimap(void){
 	glBindTexture(GL_TEXTURE_2D, minimap_tex);
 // Draw the triangle 10 times!
 	glDrawArraysInstanced(GL_TRIANGLES, 0, 6, minimap_count);
-
-=======
-
-	// two vertex buffer objects, used for uploading the
-	unsigned int vertexBufferObjID3;
-	// GL inits
-	glDisable(GL_DEPTH_TEST);
-	printError("GL inits");
-
-
-	// Load and compile shader
-	minimap_program = loadShaders("minimap.vert", "minimap.frag");
-	glUseProgram(minimap_program);
-	printError("init shader");
-
-	// Upload geometry to the GPU:
-
-	// Allocate and activate Vertex Array Object
-	glGenVertexArrays(1, &vertexArrayObjID3);
-	glBindVertexArray(vertexArrayObjID3);
-	// Allocate Vertex Buffer Objects
-	glGenBuffers(1, &vertexBufferObjID3);
-	//glGenBuffers(1, &texCoordBufferObjID);
-
-	// VBO for vertex data
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjID2);
-	glBufferData(GL_ARRAY_BUFFER, 18*sizeof(GLfloat), vertices_checkpoints, GL_STATIC_DRAW);
-
-	glVertexAttribPointer(glGetAttribLocation(minimap_program, "in_Position"), 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(glGetAttribLocation(minimap_program, "in_Position"));
-	//TEXTURE
-	printError("init vertex arrays");
-
-
-	LoadTGATextureSimple("../tex/fft-terrain-minimap-65-color.tga", &cp_tex);
-	glBindTexture(GL_TEXTURE_2D, minimap_tex);
-	glUniform1i(glGetUniformLocation(minimap_program, "tex"), 0); // Texture unit 0
-
-	printError("init arrays");
-
-}
-
-int minimap_count = 1;
-//GLfloat slope = 0.8;
-void display_billboarding_minimap(void){
-	glUseProgram(minimap_program);
-
-	glDisable(GL_CULL_FACE);
-	glDisable(GL_DEPTH_TEST);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-	mat4 trans;
-// The angle will be affected by the instance number so we pass the angle instead of  matrix.
-	trans = T(0, 0, 0);
-	glUniformMatrix4fv(glGetUniformLocation(minimap_program, "translation"), 1, GL_TRUE, trans.m);
-	//glUniform1f(glGetUniformLocation(minimap_program, "slope"), slope);
-	glBindVertexArray(vertexArrayObjID2);	// Select VAO
-	glBindTexture(GL_TEXTURE_2D, minimap_tex);
-// Draw the triangle 10 times!
-	glDrawArraysInstanced(GL_TRIANGLES, 0, 6, minimap_count);
-
->>>>>>> 51ad8beb65305cc9e7406299867c98e1f23fe563
 
 	glDisable(GL_BLEND);
 	glEnable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
-}*/
+}
 void init(void)
 {
 	int i;
